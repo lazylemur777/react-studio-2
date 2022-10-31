fd1c19d02a59cbd99ff6980cff7eb765ca9a4c6b
lazylemur777 <112660032+lazylemur777@users.noreply.github.com>
2022-10-31 13:42:50 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { useState } from "react";
+import bakeryData from "./assets/bakery-data.json";
+
+function App() {
+  // TODO: use useState to create a state variable to hold the state of the cart
+  /* add your cart state code here */
+
+  return (
+    <div className="App">
+      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+
+      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+        <p>Bakery Item {index}</p> // replace with BakeryItem component
+      ))}
+
+      <div>
+        <h2>Cart</h2>
+        {/* TODO: render a list of items in the cart */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
